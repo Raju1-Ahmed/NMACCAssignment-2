bf662bbf2c4c8ce89de73bee97034c3fcd84fee4
Raju1-Ahmed <robiussaniraju@gmail.com>
2023-07-02 15:49:18 +0600

    user signUp and login with jwt is  almost done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const brandRoute = require("./routes/Brand.route");
 const categoryRoute = require("./routes/category.route");
 const storeRoute = require("./routes/store.route");
 const supplierRoute = require("./routes/supplier.route");
+const userRoute = require("./routes/user.route");
 
 
 //middleware
@@ -24,6 +25,7 @@ app.use('/api/v1/brand', brandRoute)
 app.use('/api/v1/category',categoryRoute)
 app.use('/api/v1/store',storeRoute)
 app.use('/api/v1/supplier',supplierRoute)
+app.use('/api/v1/user', userRoute)
 
 
 module.exports = app;
---
+++ controllers/user.controller.js
@@ -0,0 +1,71 @@
+const { signupService, findUserByEmail } = require("../services/user.services")
+const bcrypt = require("bcrypt");
+const { generateToken } = require("../utils/token");
+
+exports.signup = async (req, res) => {
+    try {
+        const user = await signupService(req.body);
+        console.log("Test this is User Info:", user);
+        res.status(200).json({
+            status: "success For Create a user Account",
+            message: "Successfully signed Up",
+            data: user
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: 'fail',
+            error: error.message,
+        });
+    }
+};
+
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(401).json({
+                status: "fail",
+                error: "Please Provide Your Credentials"
+            });
+        }
+        const user = await findUserByEmail(email);
+
+        if (!user) {
+            return res.status(401).json({
+                status: "fail",
+                error: "No user found. please create an account"
+            });
+        }
+        const isPasswordValid = user.comparePassword(password, user.password)
+        if (!isPasswordValid) {
+            return res.status(403).json({
+                status: "fail",
+                error: "password is not correct"
+            });
+        }
+        if (user.status != "active") {
+            return res.status(401).json({
+                status: "fail",
+                error: "Your Account Is Not Active Yet"
+            });
+        }
+
+        const token = generateToken(user);
+
+
+        res.status(200).json({
+            status: "success",
+            message: "Successfully logged in",
+            data:{
+                user,
+                token
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: 'fail',
+            error: error.message,
+        });
+    }
+};
